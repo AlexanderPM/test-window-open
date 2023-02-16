734e64ca1d9f77027f65d19c7c902e2445c3532c
Alexander Pohozhalov <a.pohozhalov@freematiq.com>
2023-02-17 00:46:20 +0700

    тест работы window.open в асинхронном коде

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClickAsync = () => {
+      asyncFn().then((url) => {
+          window.open(url,"_blank");
+      });
+  }
+  const handleClick = () => {
+      window.open("https://www.drom.ru/","_blank");
+  }
+
+  const handleClickAsyncFixOne = () => {
+      const newWindow = window.open();
+     asyncFn().then((url) => {
+         if (newWindow)
+        newWindow.location = url;
+     }).catch(() => {
+         if (newWindow) {
+             newWindow.close();
+         }
+     });
+   }
+
+  const handleClickAsyncFixTwo = () => {
+      asyncFn().then((url) => {
+         const a = document.createElement('a');
+         a.href = url;
+         a.setAttribute('target', '_blank');
+         a.click();
+         a.remove();
+      });
+  }
+
+    const handleClickAsyncFixThree = () => {
+        asyncFn().then((url) => {
+            setTimeout(() => {
+                window.open(url,"_blank");
+            })
+        });
+    }
+
+    const handleClickAsyncFixFour = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault();
+        asyncFn().then((url) => {
+            setTimeout(() => {
+                window.open(url,"_blank");
+            })
+        });
+    }
+
+
+    const handleClickAsyncFixFive = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault();
+        let link: string | undefined = undefined;
+        asyncFn().then((url) => {
+            link = url;
+        });
+        const intervalId = setInterval(() => {
+            if (link !== undefined) {
+                clearInterval(intervalId);
+                window.open(link,"_blank");
+            }
+        },  500)
+    }
+
+  const asyncFn = async (): Promise<string> => {
+      return await new Promise((resolve) => {
+          setTimeout(() => {
+              return resolve('https://www.drom.ru/');
+          }, 5000)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <button onClick={handleClickAsync}>Перейти на другой сайт async</button>
+            <button onClick={handleClick}>Перейти на другой сайт моментально</button>
+            <button onClick={handleClickAsyncFixOne}>Перейти на другой сайт fix 1</button>
+            <button onClick={handleClickAsyncFixTwo}>Перейти на другой сайт fix 2</button>
+            <button onClick={handleClickAsyncFixThree}>Перейти на другой сайт fix 3</button>
+            <button onClick={handleClickAsyncFixFour}>Перейти на другой сайт fix 4</button>
+            <button onClick={handleClickAsyncFixFive}>Перейти на другой сайт fix 5</button>
+        </div>
+
     </div>
   );
 }
